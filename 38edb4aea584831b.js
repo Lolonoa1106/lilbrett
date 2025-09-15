8e962852603e29aba49c097e28eea35992d21581
Lolonoa1106 <ethereumsunday@gmail.com>
2025-09-14 21:32:31 -0400

    init

---
+++ 38edb4aea584831b.js
@@ -0,0 +1,11 @@
+(self["webpackChunk_canva_web"] = self["webpackChunk_canva_web"] || []).push([[2784],{
+
+/***/ 686040:
+function(_, __, __webpack_require__) {__webpack_require__.n_x = __webpack_require__.n;const __web_req__ = __webpack_require__;__web_req__(813110);self._fe4d99ebe0d2d259646a80d250150d47 = self._fe4d99ebe0d2d259646a80d250150d47 || {};(function(__c) {var gY;var dY,hgb,dgb,bgb,cgb;__c.YX=function(a){const b={...a.props},c=void 0!==a.za?a.za:a.PR;(void 0!==a.vl?a.vl:a.span).mD(({Kq:d,LD:e})=>{d=a.Jib?{}:{ah:d()};c.track(a.event,{...d,qm:e(),...b})})};__c.ZX=function(a,b,c){return 0>c?a:1<c?b:a+(b-a)*c};__c.$X=function(a){const b=new Map,c=[];for(const d of a)a=b.get(d)||0,c.push(`${d}__${a}`),b.set(d,a+1);return c};__c.aY=function(a){a=a.next();if(!a.done)return a.value};
+__c.cY=function(a){const b=({PGa:c=!1,children:d})=>{const e=bgb(),f=cgb(null);dgb(()=>egb(()=>{const {width:g,height:h,top:k=0,left:l=0}=a(),m=f.current;m&&(m.setAttribute("width",g.toString()),m.setAttribute("height",h.toString()),m.setAttribute("x",l.toString()),m.setAttribute("y",k.toString()))}),[]);return fgb(ggb,{children:[!c&&bY("clipPath",{id:e,children:bY("rect",{ref:f})}),bY("g",{clipPath:c?void 0:`url(#${e})`,children:d})]})};b.displayName=null!=a.name?`ClippingContainer(${a.name})`:"ClippingContainer";
+return b};dY=__webpack_require__(875604);hgb=dY.createContext;dgb=dY.useEffect;bgb=dY.useId;cgb=dY.useRef;var eY=__webpack_require__(443763),ggb=eY.Fragment,bY=eY.jsx,fgb=eY.jsxs;var egb=__webpack_require__(42782).EH;__c.fY={WLa:1,aNa:2,XLa:3,bMa:4,fLa:5,iNa:6};gY=__c.gY=class{static pO(a,b,c){return 0>=c?a:1<=c?b:gY.isEqual(a,b)?a:new gY(__c.ZX(a.r,b.r,c),__c.ZX(a.g,b.g,c),__c.ZX(a.b,b.b,c),__c.ZX(a.a,b.a,c))}static multiply(a,b){return gY.isEqual(a,__c.hY)?b:gY.isEqual(b,__c.hY)?a:new gY(a.r*b.r,a.g*b.g,a.b*b.b,a.a*b.a)}static isEqual(a,b){return a.r===b.r&&a.g===b.g&&a.b===b.b&&a.a===b.a}static fromHex(a){a=a.startsWith("#")?a.substr(1):a;a=parseInt(a,16);return new gY(Math.floor(a/65536)%256/255,Math.floor(a/256)%256/255,a%256/255,1)}constructor(a=0,
+b=0,c=0,d=0){this.r=a;this.g=b;this.b=c;this.a=d}};__c.iY=new gY(1,1,1,0);__c.hY=new gY(1,1,1,1);__c.jY=hgb(window);
+}).call(self, self._fe4d99ebe0d2d259646a80d250150d47);}
+
+}])
+//# sourceMappingURL=sourcemaps/38edb4aea584831b.js.map
